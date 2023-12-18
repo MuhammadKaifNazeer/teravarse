b73aa5d93a707861fa47736be6fc6218250137cb
KaifNazeer <muhammadkaifnazeer99@gmail.com>
2023-12-18 18:57:05 +0500

    added delete modal and change name of the app

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let todos = JSON.parse(localStorage.getItem('todos')) || [];
 let currentEditingId = null;
 
 function displayTodos(filteredTodos = null) {
-    const todosContainer = document.getElementById('todos');
-    todosContainer.innerHTML = '';
-  
-    const todosToDisplay = filteredTodos || todos;
-  
-    if (todosToDisplay.length === 0) {
-      const noTodosMessage = document.createElement('p');
-      noTodosMessage.className = 'text-center col-lg-12 no-todos-message';
-      noTodosMessage.textContent = 'No Todos to display here. Please add some todos.';
-      todosContainer.appendChild(noTodosMessage);
-    } else {
-      todosToDisplay.forEach(todo => {
-        const todoItem = document.createElement('div');
-        todoItem.className = `col-lg-12 col-sm-12 mb-3`;
-        
-        // Check if the task is completed and add the 'completed' class accordingly
-        const cardClass = todo.completed ? 'card completed' : 'card';
-    
-        todoItem.innerHTML = `
+  const todosContainer = document.getElementById('todos');
+  todosContainer.innerHTML = '';
+
+  const todosToDisplay = filteredTodos || todos;
+
+  if (todosToDisplay.length === 0) {
+    const noTodosMessage = document.createElement('p');
+    noTodosMessage.className = 'text-center col-lg-12 no-todos-message';
+    noTodosMessage.textContent = 'No Todos to display here. Please add some todos.';
+    todosContainer.appendChild(noTodosMessage);
+  } else {
+    todosToDisplay.forEach(todo => {
+      const todoItem = document.createElement('div');
+      todoItem.className = `col-lg-12 col-sm-12 mb-3`;
+
+      // Check if the task is completed and add the 'completed' class accordingly
+      const cardClass = todo.completed ? 'card completed' : 'card';
+
+      todoItem.innerHTML = `
           <div class="${cardClass}">
             <div class="card-body">
               <div class="d-flex justify-content-between">
@@ -28,103 +28,123 @@ function displayTodos(filteredTodos = null) {
                 <div class="d-flex">
                   <i class="fa-solid fa-pen-to-square mr-2" style="cursor: pointer;" onclick="openEditModal(${todo.id})"></i>
                   <i class="fa-solid fa-check mr-2" style="cursor: pointer;" onclick="completeTodo(${todo.id})"></i>
-                  <i class="fa-solid fa-trash" style="cursor: pointer;" onclick="deleteTodo(${todo.id})"></i>
+                  <i class="fa-solid fa-trash" style="cursor: pointer;" onclick="confirmDelete(${todo.id}, '${todo.title}')"></i>
                 </div>
               </div>
               <p class="card-text">${todo.description}</p>
             </div>
           </div>
         `;
-        todosContainer.appendChild(todoItem);
-      });
-    }
+      todosContainer.appendChild(todoItem);
+    });
   }
-  
-  
+}
+
+
 
 function filterTodos() {
-    const filter = document.getElementById('filter').value;
-    let filteredTodos = [];
+  const filter = document.getElementById('filter').value;
+  let filteredTodos = [];
 
-    if (filter === 'incomplete') {
-        filteredTodos = todos.filter(todo => !todo.completed);
-    } else if (filter === 'completed') {
-        filteredTodos = todos.filter(todo => todo.completed);
-    } else {
-        filteredTodos = todos;
-    }
+  if (filter === 'incomplete') {
+    filteredTodos = todos.filter(todo => !todo.completed);
+  } else if (filter === 'completed') {
+    filteredTodos = todos.filter(todo => todo.completed);
+  } else {
+    filteredTodos = todos;
+  }
 
-    displayTodos(filteredTodos);
+  displayTodos(filteredTodos);
 }
 
 function searchTodos() {
-    const searchKeyword = document.getElementById('search').value.toLowerCase();
-    const filteredTodos = todos.filter(todo =>
-        todo.title.toLowerCase().includes(searchKeyword) ||
-        todo.description.toLowerCase().includes(searchKeyword)
-    );
+  const searchKeyword = document.getElementById('search').value.toLowerCase();
+  const filteredTodos = todos.filter(todo =>
+    todo.title.toLowerCase().includes(searchKeyword) ||
+    todo.description.toLowerCase().includes(searchKeyword)
+  );
 
-    displayTodos(filteredTodos);
+  displayTodos(filteredTodos);
 }
 
 function addTodo() {
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
 
-    if (title && description) {
-        const newTodo = {
-            id: todos.length + 1,
-            title,
-            description,
-            completed: false
-        };
+  if (title && description) {
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      description,
+      completed: false
+    };
 
-        todos.push(newTodo);
-        localStorage.setItem('todos', JSON.stringify(todos));
-        displayTodos();
-        clearAddTodoForm();
-        $('#addTodoModal').modal('hide');
-    }
+    todos.push(newTodo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    displayTodos();
+    clearAddTodoForm();
+    $('#addTodoModal').modal('hide');
+  }
 }
 
 function clearAddTodoForm() {
-    document.getElementById('title').value = '';
-    document.getElementById('description').value = '';
+  document.getElementById('title').value = '';
+  document.getElementById('description').value = '';
 }
 
 function openEditModal(todoId) {
-    const todoToEdit = todos.find(todo => todo.id === todoId);
+  const todoToEdit = todos.find(todo => todo.id === todoId);
 
-    document.getElementById('editTitle').value = todoToEdit.title;
-    document.getElementById('editDescription').value = todoToEdit.description;
-    currentEditingId = todoId;
+  document.getElementById('editTitle').value = todoToEdit.title;
+  document.getElementById('editDescription').value = todoToEdit.description;
+  currentEditingId = todoId;
 
-    $('#editTodoModal').modal('show');
+  $('#editTodoModal').modal('show');
 }
 
 function saveEdit() {
-    const todoId = todos.findIndex(todo => todo.id === currentEditingId);
-    todos[todoId].title = document.getElementById('editTitle').value;
-    todos[todoId].description = document.getElementById('editDescription').value;
+  const todoId = todos.findIndex(todo => todo.id === currentEditingId);
+  todos[todoId].title = document.getElementById('editTitle').value;
+  todos[todoId].description = document.getElementById('editDescription').value;
 
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displayTodos();
-    $('#editTodoModal').modal('hide');
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTodos();
+  $('#editTodoModal').modal('hide');
 }
 
 function completeTodo(todoId) {
-    const todoIndex = todos.findIndex(todo => todo.id === todoId);
-    todos[todoIndex].completed = !todos[todoIndex].completed;
+  const todoIndex = todos.findIndex(todo => todo.id === todoId);
+  todos[todoIndex].completed = !todos[todoIndex].completed;
 
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displayTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTodos();
 }
 
 function deleteTodo(todoId) {
-    todos = todos.filter(todo => todo.id !== todoId);
+  todos = todos.filter(todo => todo.id !== todoId);
 
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displayTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTodos();
 }
+
+function confirmDelete(todoId, todoTitle) {
+  document.getElementById('confirmDeleteText').innerText = `Do you want to delete '${todoTitle}'?`;
+
+  $('#confirmDeleteModal').data('todo-id', todoId);
+
+  $('#confirmDeleteModal').modal('show');
+}
+
+function deleteConfirmed() {
+  const todoId = $('#confirmDeleteModal').data('todo-id');
+
+  todos = todos.filter(todo => todo.id !== todoId);
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTodos();
+
+  $('#confirmDeleteModal').modal('hide');
+}
+
 
 displayTodos();
